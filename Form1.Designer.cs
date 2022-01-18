966ec38f223834e751af2cd465777ebe64486d73
Javise46 <javierserrano2000@gmail.com>
2022-01-18 09:03:49 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Etiqueta1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Etiqueta2 = new System.Windows.Forms.Label();
+            this.Etiqueta3 = new System.Windows.Forms.Label();
+            this.Etiqueta4 = new System.Windows.Forms.Label();
+            this.Etiqueta5 = new System.Windows.Forms.Label();
+            this.Etiqueta6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(765, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iniciar Conversacion";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Etiqueta1
+            // 
+            this.Etiqueta1.AutoSize = true;
+            this.Etiqueta1.BackColor = System.Drawing.Color.Purple;
+            this.Etiqueta1.ForeColor = System.Drawing.Color.Aqua;
+            this.Etiqueta1.Location = new System.Drawing.Point(726, 9);
+            this.Etiqueta1.Name = "Etiqueta1";
+            this.Etiqueta1.Size = new System.Drawing.Size(21, 23);
+            this.Etiqueta1.TabIndex = 1;
+            this.Etiqueta1.Text = "A";
+            this.Etiqueta1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(726, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 23);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(726, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 23);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(726, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 23);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(726, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 23);
+            this.label4.TabIndex = 5;
+            // 
+            // Etiqueta2
+            // 
+            this.Etiqueta2.AutoSize = true;
+            this.Etiqueta2.BackColor = System.Drawing.Color.Black;
+            this.Etiqueta2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Etiqueta2.Location = new System.Drawing.Point(726, 43);
+            this.Etiqueta2.Name = "Etiqueta2";
+            this.Etiqueta2.Size = new System.Drawing.Size(20, 23);
+            this.Etiqueta2.TabIndex = 6;
+            this.Etiqueta2.Text = "B";
+            // 
+            // Etiqueta3
+            // 
+            this.Etiqueta3.AutoSize = true;
+            this.Etiqueta3.BackColor = System.Drawing.Color.Purple;
+            this.Etiqueta3.ForeColor = System.Drawing.Color.Aqua;
+            this.Etiqueta3.Location = new System.Drawing.Point(726, 78);
+            this.Etiqueta3.Name = "Etiqueta3";
+            this.Etiqueta3.Size = new System.Drawing.Size(21, 23);
+            this.Etiqueta3.TabIndex = 7;
+            this.Etiqueta3.Text = "A";
+            // 
+            // Etiqueta4
+            // 
+            this.Etiqueta4.AutoSize = true;
+            this.Etiqueta4.BackColor = System.Drawing.Color.Black;
+            this.Etiqueta4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Etiqueta4.Location = new System.Drawing.Point(726, 111);
+            this.Etiqueta4.Name = "Etiqueta4";
+            this.Etiqueta4.Size = new System.Drawing.Size(20, 23);
+            this.Etiqueta4.TabIndex = 8;
+            this.Etiqueta4.Text = "B";
+            // 
+            // Etiqueta5
+            // 
+            this.Etiqueta5.AutoSize = true;
+            this.Etiqueta5.BackColor = System.Drawing.Color.Purple;
+            this.Etiqueta5.ForeColor = System.Drawing.Color.Aqua;
+            this.Etiqueta5.Location = new System.Drawing.Point(726, 147);
+            this.Etiqueta5.Name = "Etiqueta5";
+            this.Etiqueta5.Size = new System.Drawing.Size(21, 23);
+            this.Etiqueta5.TabIndex = 9;
+            this.Etiqueta5.Text = "A";
+            // 
+            // Etiqueta6
+            // 
+            this.Etiqueta6.AutoSize = true;
+            this.Etiqueta6.BackColor = System.Drawing.Color.Black;
+            this.Etiqueta6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Etiqueta6.Location = new System.Drawing.Point(726, 184);
+            this.Etiqueta6.Name = "Etiqueta6";
+            this.Etiqueta6.Size = new System.Drawing.Size(20, 23);
+            this.Etiqueta6.TabIndex = 10;
+            this.Etiqueta6.Text = "B";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1269, 700);
+            this.Controls.Add(this.Etiqueta6);
+            this.Controls.Add(this.Etiqueta5);
+            this.Controls.Add(this.Etiqueta4);
+            this.Controls.Add(this.Etiqueta3);
+            this.Controls.Add(this.Etiqueta2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Etiqueta1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Etiqueta1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Etiqueta2;
+        private System.Windows.Forms.Label Etiqueta3;
+        private System.Windows.Forms.Label Etiqueta4;
+        private System.Windows.Forms.Label Etiqueta5;
+        private System.Windows.Forms.Label Etiqueta6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Etiqueta1.Text = "Hola Como Estas.";
+            Etiqueta2.Text = "Bien y Tu?";
+            Etiqueta3.Text = "Pues Bien ahi pasando el rato.";
+            Etiqueta4.Text = "Oh que bien.";
+            Etiqueta5.Text = "Me tengo que ir Adios.";
+            Etiqueta6.Text = "Yo tambien Adios.";
+           
+        }
+    }
+}
